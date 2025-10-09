4f2c84f89aa308ad5b178ff7556118efac703893
Moiz Ahmed <moeezinbox@gmail.com>
2025-10-09 11:04:53 +0530

    Added new User Control TodoView under Views folder.

---
+++ TodoList/TodoList/Views/TodoView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace TodoList;
+
+// The class name must match the x:Class in the XAML
+public partial class TodoView : UserControl
+{
+    public TodoView()
+    {
+        InitializeComponent();
+    }
+}
